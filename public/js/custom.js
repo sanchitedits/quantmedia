917bd259680543724b1985bbdc47e8b5a5da5e3b
Sanchit <sanchitpatel008@gmail.com>
2026-06-24 10:17:06 +0530

    frsgsd

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,24 +1,17 @@
-try {
-	$(function(){
-		try {
-			'use strict';
+$(function(){
+	'use strict';
 
-			// Preloader
-			var $preloader = $('#preloader');
-			if ($preloader.length) {
-				$preloader.delay(200).fadeOut('slow', function() {
-					$(this).remove();
-				});
-			}
+	// Preloader
+	var $preloader = $('#preloader');
+	if ($preloader.length) {
+		$preloader.delay(200).fadeOut('slow', function() {
+			$(this).remove();
+		});
+	}
 
-			AOS.init({
-				once: true,
-				duration: 1500,
-			});
-		} catch (e) {
-			console.error('Inner JS Error:', e);
-		}
+	AOS.init({
+	 	once: true,
+	 	duration: 1500,
 	});
-} catch (e) {
-	console.error('Custom JS Error:', e);
-}
+	
+});
